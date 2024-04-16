dc21a756f7f112663f5a36d3e5fdd58ec757bb4c
Matas Riklius <matisss1234567@gmail.com>
2024-04-16 21:45:01 +0300

    'seriuos_one_update'

--- class_material/kartojimas.py
+++ class_material/kartojimas.py
@@ -362,6 +362,7 @@ class Bus(Transport):
         return print(f'You well need {buses_needed} to export these passengers')
 
     def calculate_total_fare(self, passengers: int, distance: int, fare_per_km_per_passenger: float):
+
         total_fare = passengers * distance * fare_per_km_per_passenger
 
         return total_fare
@@ -376,12 +377,33 @@ print(bus.count_buses(130))
 print(bus.calculate_total_fare(20, 600, 0.1))
 
 
-
 class Car(Transport):
     def __init__(self, range_per_year: int, plate_number: str, fuel_type: str, expenses: int, inspection: str,
                  driving_categories: str, fuel_consumption: int, insurance_date: str):
         super().__init__(range_per_year, plate_number, fuel_type, expenses, inspection, driving_categories,
                          fuel_consumption, insurance_date)
+
+    def check_next_month_expirations(self):
+        insurance_expiry = datetime.strptime(self.insurance_date, "%Y-%m-%d")
+        current_date = datetime.now()
+
+        if current_date + timedelta(days=30) >= insurance_expiry:
+            return "You will need to do bus inspection or get insurance"
+        else:
+            return "None is needed"
+
+    def calculate_travel_costs(self, distance: int, fuel_price: float):
+        fuel_consumption_per_100_km = self.fuel_consumption
+
+        fuel_consumed = (fuel_consumption_per_100_km / 100) * distance
+
+        fuel_cost = fuel_consumed * fuel_price
+
+        fixed_costs = self.expenses
+
+        total_cost = fuel_cost + fixed_costs
+
+        return total_cost
 
 
 class Truck(Transport):
@@ -392,3 +414,25 @@ class Truck(Transport):
                          fuel_consumption, insurance_date)
         self.trailer_function = trailer_function
         self.load_capacity = load_capacity
+
+    def check_next_month_expirations(self):
+        insurance_expiry = datetime.strptime(self.insurance_date, "%Y-%m-%d")
+        current_date = datetime.now()
+
+        if current_date + timedelta(days=30) >= insurance_expiry:
+            return "You will need to do bus inspection or get insurance"
+        else:
+            return "None is needed"
+
+    def calculate_travel_costs(self, distance: int, fuel_price: float):
+        fuel_consumption_per_100_km = self.fuel_consumption
+
+        fuel_consumed = (fuel_consumption_per_100_km / 100) * distance
+
+        fuel_cost = fuel_consumed * fuel_price
+
+        fixed_costs = self.expenses
+
+        total_cost = fuel_cost + fixed_costs
+
+        return total_cost
